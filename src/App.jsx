11806a022f8ee509689e031ca1810c9b53b54964
Francisco Carvajal Villegas <57154324+FranciscoCarvajal404@users.noreply.github.com>
2023-05-05 18:52:48 -0600

    gh-pages bug resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import GlobalStyle from "./GlobalStyle"
 
 import Home from "./pages/Home"
-import Header from "./components/header"
+import Header from "./components/Header"
 import MenuHam from "./components/MenuHam"
 import PhoneNav from "./components/PhoneNav"
 
